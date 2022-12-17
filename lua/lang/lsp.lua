8fc5716798d9949efbc8971184f8526eaab03177
Staffan Wingren <mail@staffanwingren.se>
2022-12-17 22:16:51 +0100

    Modify keymap for cmp.

--- lua/lang/lsp.lua
+++ lua/lang/lsp.lua
@@ -1,6 +1,21 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+local cmp = require('cmp')
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<Tab>'] = cmp.mapping.confirm(),
+    ['<leader>c'] = cmp.mapping.abort(),
+})
+
+cmp_mappings['<S-Tab>'] = nil
+cmp_mappings['<CR>'] = nil
+cmp_mappings['<C-e>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
 lsp.setup()
 
 vim.opt.signcolumn = 'yes'
