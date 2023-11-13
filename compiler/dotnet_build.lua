20aaae4fe02837893e413c7509ca4986d5011319
Staffan Wingren <mail@staffanwingren.se>
2023-11-13 12:16:55 +0100

    Add missing files to complete kickstart setup.

---
+++ compiler/dotnet_build.lua
@@ -0,0 +1,5 @@
+vim.g.current_compiler = 'dotnet_build'
+vim.cmd([[
+    CompilerSet errorformat=%f(%l\\,%c):%\\s%t%\\w%\\+%\\s%m[%o]
+    CompilerSet makeprg=dotnet\ build\ --nologo\ -v\ q\ -clp:NoSummary\ -p:GenerateFullPaths=true
+]])
