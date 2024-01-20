caf8fed417962e475a3eed196a058a0c5486f38f
Staffan Wingren <mail@staffanwingren.se>
2024-01-20 23:47:29 +0100

    Add LSP format on <leader>gq

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,7 +23,7 @@ return {
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                 vim.keymap.set('n', '<leader>R', vim.lsp.buf.rename, opts)
                 vim.keymap.set('n', '<leader>r', vim.lsp.buf.code_action, opts)
-                --vim.keymap.set({'n', 'v'}, '<leader>rf', vim.lsp.buf.format, opts)
+                vim.keymap.set('n', '<leader>gq', vim.lsp.buf.format, opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 
@@ -32,49 +32,49 @@ return {
                 vim.api.nvim_buf_set_option(bufnr, 'tagfunc', 'v:lua.vim.lsp.tagfunc')
             end)
 
-            require('mason').setup{}
-            require('mason-lspconfig').setup{
-              ensure_installed = {
-                'gopls',
-                'lua_ls',
-                'tsserver',
-                'omnisharp',
-                'bicep',
-                'powershell_es'
-              },
-              handlers = {
-                lsp_zero.default_setup,
-                omnisharp = function()
-                    local install_dir = vim.fn.stdpath("data") .. "/mason/packages"
-                    local omnisharp_cmd = install_dir .. '/omnisharp/omnisharp.cmd'
-                    local config = {
-                        cmd = {
-                            omnisharp_cmd,
-                            '--languageserver' ,
-                            '--hostPID', tostring(vim.fn.getpid())
-                        },
-                        filetypes = { 'cs' },
-                        root_dir = require('lspconfig').util.root_pattern(
-                            '*.sln',
-                            '*.csproj',
-                            '*.props',
-                            '*.targets',
-                            'packages.config'
-                        ),
-                        enable_editorconfig_support = true,
-                        enable_roslyn_analysers = true,
-                        enable_import_completion = true,
-                        sdk_include_prereleases = true,
-                        analyze_open_documents_only = false,
-                        enable_ms_build_load_projects_on_demand = false,
-                        handlers = {
-                            ['textDocument/definition'] = require('omnisharp_extended').handler,
+            require('mason').setup {}
+            require('mason-lspconfig').setup {
+                ensure_installed = {
+                    'gopls',
+                    'lua_ls',
+                    'tsserver',
+                    'omnisharp',
+                    'bicep',
+                    'powershell_es'
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                    omnisharp = function()
+                        local install_dir = vim.fn.stdpath("data") .. "/mason/packages"
+                        local omnisharp_cmd = install_dir .. '/omnisharp/omnisharp.cmd'
+                        local config = {
+                            cmd = {
+                                omnisharp_cmd,
+                                '--languageserver',
+                                '--hostPID', tostring(vim.fn.getpid())
+                            },
+                            filetypes = { 'cs' },
+                            root_dir = require('lspconfig').util.root_pattern(
+                                '*.sln',
+                                '*.csproj',
+                                '*.props',
+                                '*.targets',
+                                'packages.config'
+                            ),
+                            enable_editorconfig_support = true,
+                            enable_roslyn_analysers = true,
+                            enable_import_completion = true,
+                            sdk_include_prereleases = true,
+                            analyze_open_documents_only = false,
+                            enable_ms_build_load_projects_on_demand = false,
+                            handlers = {
+                                ['textDocument/definition'] = require('omnisharp_extended').handler,
+                            }
                         }
-                    }
-                    require('lspconfig').omnisharp.setup(config)
-                end
-              }
-          }
+                        require('lspconfig').omnisharp.setup(config)
+                    end,
+                }
+            }
         end,
     },
 }
