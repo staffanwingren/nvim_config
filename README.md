662efe67e63a1b97cfe3b2e1a0e59ff5210f130b
Staffan Wingren <mail@staffanwingren.se>
2024-02-01 23:05:35 +0100

    Switch to project.nvim for instead of telescope-project =======================================================
    Have to figured out how to populate list of projects in dashboard yet.

--- README.md
+++ README.md
@@ -14,14 +14,3 @@ My Neovim config
   Installation: `choco install ripgrep`
 - 7Zip: For extracting, obviously, but can't remember exactly what.
 - Wget: Not sure it is needed but marked as _Error_ by `:checkhealth` if missing.
-
-## Issues
-
-### Treesitter parser installation
-
-> Treesitter is unable to find Visual Studio installation.
-
-Saw this on computer with VS2022 installed.
-
-Installed Build Tools and C++/CLI for VS2022 and, eventually, it
-started working.
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
       { '<leader>tr', ':Telescope registers<CR>',                        mode = 'n', desc = 'Telescope registers' },
       { '<leader>ts', ':Telescope lsp_workspace_symbols<CR>',            mode = 'n', desc = 'Telescope symbol search' },
       { '<leader>tt', ':Telescope resume<CR>',                           mode = 'n', desc = 'Telescope previous search' },
-      { '<leader>tp', ':Telescope project<CR>',                          mode = 'n', desc = 'Telescope project' },
+      { '<leader>tp', ':Telescope projects<CR>',                         mode = 'n', desc = 'Telescope projects' },
     },
     config = function(_, _)
       local ts = require 'telescope'
@@ -26,8 +26,53 @@ return {
         },
       }
 
-      ts.load_extension('project')
+      ts.load_extension('projects')
     end
   },
-  { 'nvim-telescope/telescope-project.nvim' }
+  {
+    'ahmedkhalf/project.nvim',
+    opts = {
+      -- Manual mode doesn't automatically change your root directory, so you have
+      -- the option to manually do so using `:ProjectRoot` command.
+      manual_mode = false,
+
+      -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+      -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+      -- order matters: if one is not detected, the other is used as fallback. You
+      -- can also delete or rearangne the detection methods.
+      detection_methods = { "lsp", "pattern" },
+
+      -- All the patterns used to detect root dir, when **"pattern"** is in
+      -- detection_methods
+      patterns = { ".git", ".sln", "Makefile", "package.json", "_darcs", ".hg", ".bzr", ".svn", },
+
+      -- Table of lsp clients to ignore by name
+      -- eg: { "efm", ... }
+      ignore_lsp = {},
+
+      -- Don't calculate root dir on specific directories
+      -- Ex: { "~/.cargo/*", ... }
+      exclude_dirs = {},
+
+      -- Show hidden files in telescope
+      show_hidden = false,
+
+      -- When set to false, you will get a message when project.nvim changes your
+      -- directory.
+      silent_chdir = false,
+
+      -- What scope to change the directory, valid options are
+      -- * global (default)
+      -- * tab
+      -- * win
+      scope_chdir = 'tab',
+
+      -- Path where project.nvim will store the project history for use in
+      -- telescope
+      datapath = vim.fn.stdpath("data"),
+    },
+    config = function(_, opts)
+      require 'project_nvim'.setup(opts)
+    end,
+  },
 }
