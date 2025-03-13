1592def017deb7ab5974ba5631ec6ec4ae0e9470
Staffan Wingren <staffan.wingren@who-umc.org>
2025-03-13 11:57:12 +0100

    Add support for fsharp and bicep

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -71,50 +71,62 @@ return {
             })
 
             local omnisharp_dll = os.getenv("OMNISHARP_DLL")
-            lspconfig["omnisharp"].setup {
-                cmd = { "dotnet", omnisharp_dll },
-                handlers = {
-                    ["textDocument/definition"] = require("omnisharp_extended").handler,
-                },
-                settings = {
-                    FormattingOptions = {
-                        -- Enables support for reading code style, naming convention and analyzer
-                        -- settings from .editorconfig.
-                        EnableEditorConfigSupport = true,
-                        -- Specifies whether 'using' directives should be grouped and sorted during
-                        -- document formatting.
-                        OrganizeImports = true,
+            if omnisharp_dll ~= nil and omnisharp_dll ~= '' then
+                lspconfig["omnisharp"].setup {
+                    cmd = { "dotnet", omnisharp_dll },
+                    handlers = {
+                        ["textDocument/definition"] = require("omnisharp_extended").handler,
                     },
-                    MsBuild = {
-                        -- If true, MSBuild project system will only load projects for files that
-                        -- were opened in the editor. This setting is useful for big C# codebases
-                        -- and allows for faster initialization of code navigation features only
-                        -- for projects that are relevant to code that is being edited. With this
-                        -- setting enabled OmniSharp may load fewer projects and may thus display
-                        -- incomplete reference lists for symbols.
-                        LoadProjectsOnDemand = nil,
+                    settings = {
+                        FormattingOptions = {
+                            -- Enables support for reading code style, naming convention and analyzer
+                            -- settings from .editorconfig.
+                            EnableEditorConfigSupport = true,
+                            -- Specifies whether 'using' directives should be grouped and sorted during
+                            -- document formatting.
+                            OrganizeImports = true,
+                        },
+                        MsBuild = {
+                            -- If true, MSBuild project system will only load projects for files that
+                            -- were opened in the editor. This setting is useful for big C# codebases
+                            -- and allows for faster initialization of code navigation features only
+                            -- for projects that are relevant to code that is being edited. With this
+                            -- setting enabled OmniSharp may load fewer projects and may thus display
+                            -- incomplete reference lists for symbols.
+                            LoadProjectsOnDemand = nil,
+                        },
+                        RoslynExtensionsOptions = {
+                            -- Enables support for roslyn analyzers, code fixes and rulesets.
+                            EnableAnalyzersSupport = true,
+                            -- Enables support for showing unimported types and unimported extension
+                            -- methods in completion lists. When committed, the appropriate using
+                            -- directive will be added at the top of the current file. This option can
+                            -- have a negative impact on initial completion responsiveness,
+                            -- particularly for the first few completion sessions after opening a
+                            -- solution.
+                            EnableImportCompletion = true,
+                            -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                            -- true
+                            AnalyzeOpenDocumentsOnly = false,
+                        },
+                        Sdk = {
+                            -- Specifies whether to include preview versions of the .NET SDK when
+                            -- determining which version to use for project loading.
+                            IncludePrereleases = true,
+                        },
                     },
-                    RoslynExtensionsOptions = {
-                        -- Enables support for roslyn analyzers, code fixes and rulesets.
-                        EnableAnalyzersSupport = true,
-                        -- Enables support for showing unimported types and unimported extension
-                        -- methods in completion lists. When committed, the appropriate using
-                        -- directive will be added at the top of the current file. This option can
-                        -- have a negative impact on initial completion responsiveness,
-                        -- particularly for the first few completion sessions after opening a
-                        -- solution.
-                        EnableImportCompletion = true,
-                        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-                        -- true
-                        AnalyzeOpenDocumentsOnly = false,
-                    },
-                    Sdk = {
-                        -- Specifies whether to include preview versions of the .NET SDK when
-                        -- determining which version to use for project loading.
-                        IncludePrereleases = true,
-                    },
-                },
-            }
+                }
+            end
+            --if vim.fn.executable('fsautocomplete') == 1 then
+            --    lspconfig["fsautocomplete"].setup{}
+            --end
+
+            local bicep_dll = os.getenv("BICEP_LANGSRV_DLL")
+            if bicep_dll ~= nil and bicep_dll ~= '' then
+                lspconfig["bicep"].setup {
+                    cmd = { "dotnet", bicep_dll },
+                }
+            end
 
             if vim.fn.executable('lua-language-server') == 1 then
                 lspconfig["lua_ls"].setup{}
@@ -124,5 +136,12 @@ return {
                 lspconfig['marksman'].setup{}
             end
         end,
+    },
+    {
+        "ionide/Ionide-vim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+        },
+        ft = { "fsharp" },
     }
 }
